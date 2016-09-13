8c3d844edd9d141519100e03a19c9f8f720c0cfc
Said El Dah <saeed.eldah@gmail.com>
2016-09-13 23:53:19 +0300

    Add project files.

---
+++ ExtensionMethodExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
